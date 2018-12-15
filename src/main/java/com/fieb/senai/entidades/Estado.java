345d140c3081467eee040d4c17c7f8e21c5dade0
denival.lima@gmail.com <denival.lima@gmail.com>
2018-12-14 22:33:18 -0200

    terceiro commit

--- src/main/java/com/fieb/senai/entidades/Estado.java
+++ src/main/java/com/fieb/senai/entidades/Estado.java
@@ -1,7 +1,11 @@
 
 package com.fieb.senai.entidades;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -9,7 +13,7 @@ import java.util.Set;
 public class Estado {
     
     private String nome;
-    Set<Integer> cidades = new HashSet<>();
+    List<Integer> cidades = new ArrayList<>();
 
     public Estado(String nome) {
         this.nome = nome;
@@ -32,6 +36,16 @@ public class Estado {
         hash = 29 * hash + Objects.hashCode(this.nome);
         return hash;
     }
+
+    public List<Integer> getCidades() {
+        return Collections.unmodifiableList(cidades);
+    }
+
+    public void setCidades(Integer... cidades) {
+        this.cidades.addAll(Arrays.asList(cidades));
+    }
+    
+    
 
     @Override
     public boolean equals(Object obj) {
